e5a9bf3319929c081ace8745d0e2ab4ece424e7e
sagar basavaraj <sagarbasavaraj@sagars-MBP.local>
2019-02-11 23:15:26 +0100

    initial commit with setup (added styled-components)

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 40px;
+`;
+
+const Index = () => (
+  <header>
+    <Title>Work in progress....</Title>
+  </header>
+);
+
+export default Index;
